e6bdec03d0dd938ee8f9c53e0cfdd524850bfe23
Chiragshah1110 <chiragshah0784@gmail.com>
2024-01-13 16:20:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+alert("Welcome! Hope you find good interships")
+const togglebtn=document.querySelector('.toggle_btn')
+const togglebtnicon=document.querySelector('.toggle_btn i')
+const dropDownMenu=document.querySelector('.dropdown_mwnu')
